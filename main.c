c0dcb60a09504ff2c22c25b6d3abdf3eaf201dd2
pc leo <analistaleonardosantana55@gmail.com>
2024-02-28 13:35:26 -0300

    event loop

--- main.c
+++ main.c
@@ -1,25 +1,23 @@
-// Example program:
-// Using SDL2 to create an application window
-
+#include <windows.h>
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <stdbool.h>
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) {
 
-    SDL_Window *window=NULL;                    // Declare a pointer
+    SDL_Window *window=NULL;
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    SDL_Init(SDL_INIT_VIDEO);
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "An SDL2 window",
-        100,
-        100,                  // window title
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_SHOWN                  // flags - see below
+        "An SDL2 window",     // window title
+        100,                  // where it opens ins the screen
+        100,                   
+        640,                  // width, in pixels
+        480,                  // height, in pixels
+        SDL_WINDOW_SHOWN      // flags - open_gl, etc
     );
 
     // Check that the window was successfully created
@@ -29,9 +27,38 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmd
         return 1;
     }
 
-    // The window is open: could enter program loop here (see SDL_PollEvent())
+    // Game loop
+    bool gameIsRunning = true;
+    while(gameIsRunning){
+        //all events get stored here before the loop continues
+        SDL_Event sdl_event;
+        while(SDL_PollEvent(&sdl_event)){
+            
+            if (sdl_event.type == SDL_QUIT){
+            gameIsRunning = false;
+            }
 
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+            if (sdl_event.type == SDL_KEYDOWN){
+                if (sdl_event.key.keysym.scancode == SDL_SCANCODE_DOWN){
+                    printf("down");
+                }
+                if (sdl_event.key.keysym.scancode == SDL_SCANCODE_UP){
+                    printf("up");
+                }
+                if (sdl_event.key.keysym.scancode == SDL_SCANCODE_LEFT){
+                    printf("left");
+                }
+                if (sdl_event.key.keysym.scancode == SDL_SCANCODE_RIGHT){
+                    printf("right");
+                }
+
+            }
+
+        }
+
+
+    }
+    // The window is open: could enter program loop here (see SDL_PollEvent())
 
     // Close and destroy the window
     SDL_DestroyWindow(window);
